8105f1ccd97d9fd304017a4380a43e5104e19971
Saing Sab <saing.sab@gmail.com>
2021-01-25 16:51:16 +0700

    health_check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use asrtemplate::run;
+use std::net::TcpListener;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let address = format!("127.0.0.1:{}", "8088");
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,24 +1,32 @@
+use std::net::TcpListener;
+use asrtemplate::run;
+
+
+ fn spawn_app() {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
 
 // #[actix_rt::test]
-// async fn health_check_works(){
-//     // spawn_app().await.expect("Failed to spawn our app.");
-//     spawn_app();
+// async fn health_check_works() {
+//     // Arrange
+//     let app = spawn_app().await;
 //     let client = reqwest::Client::new();
 
-//     let response = client 
-//         .get("http://127.0.0.1:8088/health_check")
+//     // Act
+//     let response = client
+//         // Use the returned application address
+//         .get(&format!("{}/health_check", &app.address))
 //         .send()
 //         .await
 //         .expect("Failed to execute request.");
 
-//         // Assert
-//         assert!(response.status().is_success());
-//         assert_eq!(Some(0), response.content_length());
-
-// }
-
-//  fn spawn_app() {
-    
-//    let server = asrtemplate::run().expect("Failed to bind address");
-//    let _ = tokio::spawn(server);
+//     // Assert
+//     assert!(response.status().is_success());
+//     assert_eq!(Some(0), response.content_length());
 // }
