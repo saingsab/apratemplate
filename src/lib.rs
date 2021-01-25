b6f6665f4b2f74bee5e2617ea7bd0f57aefec312
Saing Sab <saing.sab@gmail.com>
2021-01-25 12:20:06 +0700

    running server

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,15 @@ use actix_web::dev::Server;
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
 pub fn run() -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
         .route("/health_check", web::get().to(health_check))
     })        
-    .bind("127.0.0.1:8000")?        
+    .bind("127.0.0.1:8088")?        
     .run();
+    // .await
     // No .await here!
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,24 +1,24 @@
-use asrtemplate::main;
-use tokio::*;
 
 #[actix_rt::test]
-async fn health_check_works() {
-    spawn_app();
-
+async fn health_check_works(){
     // spawn_app().await.expect("Failed to spawn our app.");
-    // let client = reqest::Client::new();
+    spawn_app();
+    let client = reqwest::Client::new();
 
-    // let response = client
-    //     .get("http://127.0.0.1:8800/health_check")
-    //     .send()
-    //     .await
-    //     .expect("Failed to execute request.");
+    let response = client 
+        .get("http://127.0.0.1:8088/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
-    // assert!(response.status().is_success());
-    // assert_eq!(Some(0), response.content_length());
+        // Assert
+        assert!(response.status().is_success());
+        assert_eq!(Some(0), response.content_length());
+
 }
 
-async fn spawn_app() {
-    let server = asrtemplate::run().expect("Failed to bind address");
-    let _ = tokio::spawn(server);
+ fn spawn_app() {
+    
+   let server = asrtemplate::run().expect("Failed to bind address");
+   let _ = tokio::spawn(server);
 }
