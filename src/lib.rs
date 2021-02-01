6864bd2e7edab5a0a709da022c8cceb669e3ff32
Saing Sab <saing.sab@gmail.com>
2021-02-01 15:36:17 +0700

    add hard configuration file

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
@@ -7,15 +7,14 @@ async fn health_check() -> HttpResponse {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-        .route("/health_check", web::get().to(health_check))
-    })        
-    .listen(listener)?        
-    .run();
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
     // .await
     // No .await here!
     Ok(server)
 }
 
-
+pub mod configuration;
+pub mod routes;
+pub mod startup;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use asrtemplate::configuration::get_configuration;
 use asrtemplate::run;
 use std::net::TcpListener;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let address = format!("127.0.0.1:{}", "8088");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration file");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
